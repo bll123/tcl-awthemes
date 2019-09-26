2948196b8ddf6a72abd2132140c7b7458d1b7991
Brad Lanam <brad.lanam.di@gmail.com>
2019-09-26 13:52:15 -0700

    awthemes 2.6

---
+++ pkgIndex.tcl
@@ -0,0 +1,6 @@
+package ifneeded awdark 2.6 \
+    [list source [file join $dir awthemes.tcl]]
+package ifneeded awlight 2.6 \
+    [list source [file join $dir awthemes.tcl]]
+package ifneeded colorutils 4.3 \
+    [list source [file join $dir colorutils.tcl]] 
