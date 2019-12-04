a0eb8be5ad07aa81d13ddd1f3712b5998d25f214
Brad Lanam <brad.lanam.di@gmail.com>
2019-12-04 07:21:14 -0800

    update pkgindex

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,13 +1,13 @@
-package ifneeded awthemes 7.3 \
+package ifneeded awthemes 7.5 \
     [list source [file join $dir awthemes.tcl]] 
-package ifneeded colorutils 4.4 \
+package ifneeded colorutils 4.5 \
     [list source [file join $dir colorutils.tcl]] 
 
-package ifneeded awdark 7.0 \
+package ifneeded awdark 7.1 \
     [list source [file join $dir awdark.tcl]]
-package ifneeded awlight 7.0 \
+package ifneeded awlight 7.1 \
     [list source [file join $dir awlight.tcl]]
 package ifneeded black 7.0 \
     [list source [file join $dir awblack.tcl]]
-package ifneeded winxpblue 7.0 \
+package ifneeded winxpblue 7.1 \
     [list source [file join $dir awwinxpblue.tcl]]
