21c4ae0ec1429e6131ff35c1b1d8bc68976d898b
Brad Lanam <brad.lanam.di@gmail.com>
2020-04-27 10:40:51 -0700

    9.1

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,15 +1,17 @@
-package ifneeded awthemes 9.0 \
+package ifneeded awthemes 9.1 \
     [list source [file join $dir awthemes.tcl]]
 package ifneeded colorutils 4.7 \
     [list source [file join $dir colorutils.tcl]]
 
-package ifneeded awdark 7.6 \
+package ifneeded arc 1.0 \
+    [list source [file join $dir awarc.tcl]]
+package ifneeded awdark 7.7 \
     [list source [file join $dir awdark.tcl]]
-package ifneeded awlight 7.5 \
+package ifneeded awlight 7.6 \
     [list source [file join $dir awlight.tcl]]
-package ifneeded black 7.4 \
+package ifneeded black 7.5 \
     [list source [file join $dir awblack.tcl]]
-package ifneeded winxpblue 7.5 \
-    [list source [file join $dir awwinxpblue.tcl]]
-package ifneeded breeze 1.3 \
+package ifneeded breeze 1.4 \
     [list source [file join $dir awbreeze.tcl]]
+package ifneeded winxpblue 7.6 \
+    [list source [file join $dir awwinxpblue.tcl]]
