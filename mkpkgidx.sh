6fe4d7d647b43cbca697832e7fc99b28ae16326a
Brad Lanam <brad.lanam.di@gmail.com>
2020-10-23 10:02:29 -0700

    fix mkpkgidx.sh

--- mkpkgidx.sh
+++ mkpkgidx.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 > pkgIndex.tcl
-for f in awthemes.tcl colorutils.tcl awarc.tcl awdark.tcl awlight.tcl \
+for f in awthemes.tcl ; do
+  sf=$(echo $f | sed 's/\.tcl$//')
+  pp=$(grep 'set ::themeutils::awversion [0-9]' $f |
+    sed -e 's/.* /package ifneeded /')
+  cat << _HERE_ >> pkgIndex.tcl
+$pp \\
+    [list source [file join \$dir $f]]
+_HERE_
+done
+
+for f in colorutils.tcl awarc.tcl awdark.tcl awlight.tcl \
     awblack.tcl awbreeze.tcl awwinxpblue.tcl awclearlooks.tcl \
     awtemplate.tcl; do
   sf=$(echo $f | sed 's/\.tcl$//')
   pp=$(grep 'package provide [a-z]* [0-9]' $f |
-    sed -e 's/provide/ifneeded/')
+    sed -e 's/provide/ifneeded/;s/^ *//')
   cat << _HERE_ >> pkgIndex.tcl
 $pp \\
     [list source [file join \$dir $f]]
